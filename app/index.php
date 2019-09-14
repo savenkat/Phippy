da0f373581deeb3a179819b07f311436b868cfdb
Shashank Barsin <shashankbarsin@gmail.com>
2019-09-14 17:59:06 +0530

    Ingress and secret

--- app/index.php
+++ app/index.php
@@ -1,9 +1,12 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<h1 style="color:grey;">Hello everyone. Good morning</h1>' ; ?> 
- <img src="https://www.cncf.io/wp-content/uploads/2018/12/phipp5-01.svg" alt="Phippy">
- </body>
+	<head>
+		<title>PHP Test</title>
+	</head>
+	<body>
+		<?php 
+			$name = getenv('NAME');
+			echo '<h1 style="color:grey;">Good morning '.$name.'</h1>'; 
+		?>
+		<img src="https://www.cncf.io/wp-content/uploads/2018/12/phipp5-01.svg" alt="Phippy" width="500" height="500">
+	</body>
 </html>
--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<h1 style="color:grey;">Hello everyone. Good morning</h1>' ; ?> 
- <img src="https://www.cncf.io/wp-content/uploads/2018/12/phipp5-01.svg" alt="Phippy">
- </body>
-</html>
